27e40cc52a0ce01868d3312363bddc2d228afb6f
Marco Palayad <marco.palayad@gmail.com>
2026-01-15 20:43:55 +0800

    generate html UI using JS

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,37 @@
-const products = [{
-	image: 'mages/products/athletic-cotton-socks-6-pairs.jpg',
-	name: 'Black and Gray Athletic Cotton Socks - 6 Pairs<',
-	rating: {
-		stars: 4.5,
-		count: 87
+const products = [
+	{
+		image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+		name: "Black and Gray Athletic Cotton Socks - 6 Pairs<",
+		rating: {
+			stars: 4.5,
+			count: 87,
+		},
+		priceCents: 1090,
 	},
-	priceCents: 1090
-}, {
-	image: "mages/products/intermediate-composite-basketball.jpg",
-	name: "Intermediate Size Basketball",
-	rating: {
-		stars: 4,
-		count: 127
+	{
+		image: "images/products/intermediate-composite-basketball.jpg",
+		name: "Intermediate Size Basketball",
+		rating: {
+			stars: 4,
+			count: 127,
+		},
+		priceCents: 2095,
 	},
-	priceCents: 2095
-}, {
-	image: "mages/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-	name: "Adults Plain Cotton T-Shirt - 2 Pack",
-	rating: {
-		stars: 4.5,
-		count: 56
+	{
+		image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+		name: "Adults Plain Cotton T-Shirt - 2 Pack",
+		rating: {
+			stars: 4.5,
+			count: 56,
+		},
+		priceCents: 799,
 	},
-	priceCents: 799
-	}];
+];
 
+let productsHTML = "";
 
-products.forEach((product) => { 
-	const html = `
+products.forEach((product) => {
+	productsHTML += `
 		<div class="product-container">
 			<div class="product-image-container">
 				<img class="product-image" src="${product.image}" />
@@ -36,10 +41,10 @@ products.forEach((product) => {
 
 			<div class="product-rating-container">
 				<img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png" />
-				<div class="product-rating-count link-primary">${products.rating.count}</div>
+				<div class="product-rating-count link-primary">${product.rating.count}</div>
 			</div>
 
-			<div class="product-price">${product.priceCents / 100}</div>
+			<div class="product-price">${(product.priceCents / 100).toFixed(2)}</div>
 
 			<div class="product-quantity-container">
 				<select>
@@ -65,8 +70,9 @@ products.forEach((product) => {
 
 			<button class="add-to-cart-button button-primary">Add to Cart</button>
 		</div>
-	`
-})
+	`;
+});
 
+console.log(products);
 
-
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
